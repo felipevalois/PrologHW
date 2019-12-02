6a05bd277448471fd610b36f53ef62bca8499513
Felipe Costa <36748291+felipevalois@users.noreply.github.com>
2019-12-02 14:33:06 -0600

    Update hw2.pl

--- hw2.pl
+++ hw2.pl
@@ -4,7 +4,7 @@
  * Homework 2
  */
 
-% https://swish.swi-prolog.org/
+% https://swish.swi-prolog.org/p/fvc2m9.pl
 
 % 1. (10 points) Given a positive integer N, print ‘hello world’ N times.
 hello_world(X) :-
@@ -12,6 +12,7 @@ hello_world(X) :-
             write('Hello World\n')).
 
 % 2. (15 points) Find the last element of a list; if the list is empty, return ‘NULL’.
+last_element([],'NULL').
 last_element([Head], Y) :-
 	Y=Head.
 	last_element([_|Tail], Y) :-
@@ -28,25 +29,25 @@ is_prime(X) :- not(divisible(X,2)).
 % 4. (25 points) Return the Least common multiple for all integers in a given list.
 %    (For simplicity, all of those integers are assumed to be positive)
 lcm([],1).
-lcm([X|Y], P) :- lcm(Y,Q),P is X*Q/gcd(X,Q).
-
+% lcm is the product of the numbers divided by their gcd
+lcm([X|Y], P) :-
+    % recursive call removing head each time
+    lcm(Y,Q),
+    P is X*Q/gcd(X,Q).
 
 % 5. (30 points) Return the factorials of all prime numbers between two integers.
 %    (Here closed interval is used to confine the range, like [2, 9].)
 
 rangeToList(L,H,A) :-
     L =< H,
-    X is L+1,
-    Y is H-1,
-    X \= Y,
-    findall(Z,between(X,Y,Z),A).
+    findall(Z,between(L,H,Z),A).
 
 factorial(0,1).
 factorial(N,F) :- N>0, N1 is N-1, factorial(N1,F1), F is N*F1.
 
 primes_factorial(L,H,Y) :-
     rangeToList(L,H,Z),
-    % checking if each item is prime, then X contains prime number from given list
+    % checking if each item is prime, then X contains only the prime numbers from given list
     include(is_prime, Z, X),
-    % applying factorial to each item
+    % applying factorial to each prime num
 	maplist(factorial, X, Y).
